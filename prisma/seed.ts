29c0990bc426c20aa5ec69e4c794dea6f3d0d2bc
Ian Farai Madhara <iannyfarai@gmail.com>
2025-12-26 23:24:47 -0500

    added seeding script

---
+++ prisma/seed.ts
@@ -0,0 +1,64 @@
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: "iannyfarai@gmail.com",
+    name: "Ian Farai Madhara",
+    phoneNumber: "2017364158",
+    address: {
+      create: {
+        streetAddress: "13107 160th",
+        city: "Jamaica",
+        state: "New York",
+        zipCode: "13107",
+        country: "United States",
+      },
+    },
+    dropoff: {
+      create: {
+        separateWash: true,
+        handDried: true,
+        notes: "Please do not use any detergent",
+      },
+    },
+  },
+  {
+    email: "naomimaguraz@gmail.com",
+    name: "Naomi Madhara",
+    phoneNumber: "2017364159",
+    address: {
+      create: {
+        streetAddress: "13107 160th",
+        city: "Brooklyn",
+        state: "New York",
+        zipCode: "11007",
+        country: "United States",
+      },
+    },
+    dropoff: {
+      create: {
+        separateWash: true,
+        handDried: false,
+        notes: "Please do not use any detergent",
+      },
+    },
+  },
+];
+
+export async function main() {
+  for (const u of userData) {
+    await prisma.user.create({ data: u });
+  }
+}
+
+main();
